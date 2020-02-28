6de06e285a486242523e596d39f06dc2cfcb28e0
Mike.Mueller <michael17mueller@gmail.com>
2020-02-27 23:38:35 -0500

    update - game will now render properly via url id

--- src/pages/game/Game.component.jsx
+++ src/pages/game/Game.component.jsx
@@ -15,39 +15,46 @@ export class Game extends Component {
 	}
 
 	componentDidMount = () => {
-		axios({
-			url: `${this.proxy}https://api-v3.igdb.com/games/`,
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'user-key': '2eb22685103f241e827ecc8d80a37538'
-			},
-			data: `fields *; where id = ${this.props.match.params.id};`
-		})
-			.then((response) => {
-				axios({
-					url: `${this.proxy}https://api-v3.igdb.com/covers/`,
-					method: 'POST',
-					headers: {
-						Accept: 'application/json',
-						'user-key': '2eb22685103f241e827ecc8d80a37538'
-					},
-					data: `where id = ${response.data[0].cover}; fields *;`
-				})
-					.then((res) => {
-						console.log(response.data);
-						this.setState({
-							resp: response.data[0],
-							img: res.data[0].image_id
-						});
-					})
-					.catch((err) => {
-						console.error(err);
-					});
+		if (!this.props.location.state) {
+			axios({
+				url: `${this.proxy}https://api-v3.igdb.com/games/`,
+				method: 'POST',
+				headers: {
+					Accept: 'application/json',
+					'user-key': '2eb22685103f241e827ecc8d80a37538'
+				},
+				data: `fields *; where id = ${this.props.match.params.id};`
 			})
-			.catch((err) => {
-				console.error(err);
+				.then((response) => {
+					axios({
+						url: `${this.proxy}https://api-v3.igdb.com/covers/`,
+						method: 'POST',
+						headers: {
+							Accept: 'application/json',
+							'user-key': '2eb22685103f241e827ecc8d80a37538'
+						},
+						data: `where id = ${response.data[0].cover}; fields *;`
+					})
+						.then((res) => {
+							console.log(response.data);
+							this.setState({
+								resp: response.data[0],
+								img: `https://images.igdb.com/igdb/image/upload/t_cover_big/${res.data[0].image_id}.jpg`
+							});
+						})
+						.catch((err) => {
+							console.error(err);
+						});
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		} else {
+			this.setState({
+				resp: { name: this.newProps.name, id: this.newProps.gameID },
+				img: this.newProps.img
 			});
+		}
 	};
 
 	render() {
@@ -55,15 +62,10 @@ export class Game extends Component {
 			<div className='Game'>
 				<header className='Game--header'>
 					<h2>{this.state.resp.name}</h2>
-					<h2>{this.state.resp.id}</h2>
 				</header>
 				<main className='Game--main'>
 					<div className='Game--main--cover'>
-						<img
-							className='Game--main--cover--img'
-							src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${this.state.img}.jpg`}
-							alt='cover of game'
-						/>
+						<img className='Game--main--cover--img' src={this.state.img} alt='cover of game' />
 					</div>
 				</main>
 			</div>
